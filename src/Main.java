85a66ac679b2d347b2ef4f0bae50216193683036
vorona_af <vorona_af@magnit.ru>
2025-04-03 21:00:08 +0300

    6 ДЗ правки стиля

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-import manager.Managers;
-import manager.TaskManager;
-import task.Epic;
-import task.Status;
-import task.Subtask;
-import task.Task;
-
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 }
